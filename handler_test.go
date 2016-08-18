ac71dba4b5ccd01f2ae1149251f8c56b10df8d00
Johannes Scheuermann <johannes.scheuermann@inovex.de>
2016-08-18 23:47:18 +0200

    Add timeout to HTTP Client

--- handler_test.go
+++ handler_test.go
@@ -8,9 +8,16 @@ import (
 	"net/http"
 	"reflect"
 	"testing"
+	"time"
 )
 
 var todoAppServer = "http://localhost:3000"
+
+func getHTTPClient() *http.Client {
+	return &http.Client{
+		Timeout: time.Duration(30 * time.Second),
+	}
+}
 
 //TestHealthEndpoint checks if the Health endpoint has the right format
 func TestHealthEndpoint(t *testing.T) {
@@ -20,7 +27,7 @@ func TestHealthEndpoint(t *testing.T) {
 		"self":         "ok",
 	}
 
-	resp, err := http.Get(fmt.Sprintf("%s/health", todoAppServer))
+	resp, err := getHTTPClient().Get(fmt.Sprintf("%s/health", todoAppServer))
 	if err != nil {
 		t.Log(err)
 		t.FailNow()
@@ -42,13 +49,13 @@ func TestInsertReadAndDeleteItem(t *testing.T) {
 	insertItem := "TestCase"
 
 	// Insert Item
-	if _, err := http.Get(fmt.Sprintf("%s/insert/todo/%s", todoAppServer, insertItem)); err != nil {
+	if _, err := getHTTPClient().Get(fmt.Sprintf("%s/insert/todo/%s", todoAppServer, insertItem)); err != nil {
 		t.Log(err)
 		t.FailNow()
 	}
 
 	// Read Item
-	readResp, err := http.Get(fmt.Sprintf("%s/read/todo", todoAppServer))
+	readResp, err := getHTTPClient().Get(fmt.Sprintf("%s/read/todo", todoAppServer))
 	if err != nil {
 		t.Log(err)
 		t.FailNow()
@@ -66,7 +73,7 @@ func TestInsertReadAndDeleteItem(t *testing.T) {
 	}
 
 	// Delete Item
-	deleteResp, err := http.Get(fmt.Sprintf("%s/delete/todo/%s", todoAppServer, insertItem))
+	deleteResp, err := getHTTPClient().Get(fmt.Sprintf("%s/delete/todo/%s", todoAppServer, insertItem))
 	if err != nil {
 		t.Log(err)
 		t.FailNow()
