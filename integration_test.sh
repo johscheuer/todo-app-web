bfe8425aecad736b258cf2aa0fd1d3e1ce7930e3
Johannes Scheuermann <johannes.scheuermann@inovex.de>
2016-08-19 00:06:08 +0200

    Test for travis

--- integration_test.sh
+++ integration_test.sh
@@ -2,8 +2,13 @@
 
 docker-compose up -d
 
+docker-compose ps
+
+curl http://localhost:3000/health
+
 go test -v -tags integration
 
+[ $? -eq 0 ] || exit $?;
 # Clean up
 docker-compose kill
 docker-compose rm -f
